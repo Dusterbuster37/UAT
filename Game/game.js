4614c9d7635a9949084d046a30570599b6110ebb
Dusterbuster37 <139425685+Dusterbuster37@users.noreply.github.com>
2023-09-12 13:47:47 -0700

    Add files via upload

--- Game/game.js
+++ Game/game.js
@@ -1,5 +1,5 @@
 //game
-// Function to generate a random number between min and max (inclusive)
+// Function to generate a random number between min and max
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
